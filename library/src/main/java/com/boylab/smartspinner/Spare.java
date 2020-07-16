7bd61c0bf56acd0b1d9f09f630e79484f91f27e4
pengle <pengle609@163.com>
2020-07-16 08:38:13 +0800

    123

--- library/src/main/java/com/boylab/smartspinner/Spare.java
+++ library/src/main/java/com/boylab/smartspinner/Spare.java
@@ -37,22 +37,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/*
- * Copyright (C) 2015 Angelo Marchesin.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * 备用
  */
-public class SmartSpinner2 extends AppCompatTextView {
+
+@Deprecated
+public class Spare extends AppCompatTextView {
 
     private static final int MAX_LEVEL = 10000;
     private static final int VERTICAL_OFFSET = 1;
@@ -121,17 +111,17 @@ public class SmartSpinner2 extends AppCompatTextView {
     @Nullable
     private ObjectAnimator arrowAnimator = null;
 
-    public SmartSpinner2(Context context) {
+    public Spare(Context context) {
                 super(context);
         init(context, null);
     }
 
-    public SmartSpinner2(Context context, AttributeSet attrs) {
+    public Spare(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
 
-    public SmartSpinner2(Context context, AttributeSet attrs, int defStyleAttr) {
+    public Spare(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context, attrs);
     }
